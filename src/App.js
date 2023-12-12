d43ed590bf16c7ede298a2c7f3af77c760bd0430
Anton Fefilov <a.fefilov@astondevs.ru>
2023-12-12 16:06:35 +0300

    smth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+  CardHeader,
+  Button,
+} from "@mui/material";
+import { congrats } from "./consts";
+import { useState } from "react";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const onNextBtnClick = () => {
+    setIndex(index + 1);
+  };
+
+  console.log(index);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>С днем рождения Даша!</p>
+        {congrats.map(({ name, text, color }, i) => (
+          <Card
+            key={text}
+            sx={{ minWidth: 275 }}
+            style={index === i ? { display: "block" } : { display: "none" }}
+          >
+            <CardHeader
+              title={name}
+              subheader="13/12/2023"
+              avatar={
+                <Avatar
+                  src="/public/ann.jpg" 
+                  sx={{ "background-color": color }}
+                >{`${name[0]}`}</Avatar>
+              }
+            ></CardHeader>
+            <CardContent>
+              <Typography
+                variant="body2"
+                style={{ fontSize: '18px', fontWeight: '400', fontStyle: 'Regular' }}
+              >
+                {text}
+                <br />
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+        <Button onClick={onNextBtnClick}>Next</Button>
       </header>
     </div>
   );
---
+++ src/consts.js
@@ -0,0 +1,32 @@
+export const congrats = [
+  {
+    name: "Boss",
+    text: "С др!",
+    color: "rgb(255, 87, 34)",
+  },
+  {
+    name: "Ann",
+    text: "Поздравляю.",
+    color: "rgb(103, 58, 183)",
+  },
+  {
+    name: "Vitaly",
+    text: "Поздравляю.",
+    color: "rgb(8, 92, 37)",
+  },
+  {
+    name: "Dim",
+    text: "Поздравляю.",
+    color: "rgb(31, 108, 250)",
+  },
+  {
+    name: "Dasha",
+    text: "Поздравляю.",
+    color: "rgb(253, 126, 151)",
+  },
+  {
+    name: "Tony",
+    text: "Поздравляю.",
+    color: "rgb(189, 189, 189)",
+  },
+];
